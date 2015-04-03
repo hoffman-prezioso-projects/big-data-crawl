9a9b050a373ac796f3005ad1d6680cf6691c54a9
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-04-03 12:49:50 -0400

    Add warc_to_text.py

---
+++ src/warc_to_text.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+from hanzo import warctools
+import codecs
+import re
+import sys
+
+from html_parser import strip_html
+
+if not sys.argv[1]:
+  print "Please supply warc.gz file"
+  sys.exit(1)
+
+if not sys.argv[2]:
+  print "please supply an output directory"
+  sys.exit(2)
+
+warcStream = warctools.WarcRecord.open_archive(sys.argv[1])
+outputDir = sys.argv[2]
+regex = re.compile(r'\W+')
+
+i = 0 #for testing
+
+for record in warcStream:
+  if record.type == "response":
+    if record.content[1][0:50].find("HTTP/1.1 2") > -1:	#if a succesful response
+      startIndex = record.content[1].find("<html")
+      if startIndex > -1:	#if html found
+        
+        filename = re.sub('[<>]', '', record.id)
+        filename = re.sub(':', '-', filename)
+        fileName = outputDir + '/' + filename #record.url.replace('/', '>')
+        
+        fileObject = codecs.open(fileName, "w", "utf-8")
+        fileObject.write(record.url + ' ')
+        
+        words = strip_html(record.content[1][startIndex:-1])
+        for word in words:
+          try:
+            utf8_word = word.decode('utf-8')
+            fileObject.write(utf8_word + ' ')
+          except UnicodeDecodeError:
+            pass
+        fileObject.close()
+        i = i + 1
+        if i % 10 == 0:
+          print '%6s records processed' % (i) 
+        
+	
+
+print 'Plain Text Files Have Been Extracted'
