aec36e4668fed0a1606fafae7c7fd7ba193a14e7
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-04-03 15:49:44 -0400

    Add mapper

---
+++ src/mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+
+  line = line.strip()
+  words = line.split()
+	
+  # separate url and words
+  url = words[0]
+  words = words[1:]
+	
+  # compute word frequencies
+  word_frequencies = {}
+  for word in words:
+    if word in word_frequencies:
+      word_frequencies[word] += 1
+    else:
+      word_frequencies[word] = 1
+  
+  # emit word and its frequecy
+  for word in word_frequencies:
+		print '%s\t%s,%s' % (word, url, word_frequencies[word])
