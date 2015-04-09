1acb2abbdb21531a4167e6f1ec33cad9fc654b30
Stefano Prezioso <stefprez@gmail.com>
2015-04-09 17:12:48 -0400

    Refactored variable names to use underscores vs camel case per PEP-8.

--- src/warc_to_text.py
+++ src/warc_to_text.py
@@ -25,21 +25,21 @@ if len(sys.argv) < 5:
 else:
     records_per_file = int(sys.argv[4])
 
-warcStream = warctools.WarcRecord.open_archive(sys.argv[1])
-outputDir = sys.argv[2]
+warc_stream = warctools.WarcRecord.open_archive(sys.argv[1])
+output_directory = sys.argv[2]
 regex = re.compile(r'\W+')
 
-i = 0
+file_name_counter = 0
 
-for record in warcStream:
-    if i % records_per_file == 0:
-        fileObject = codecs.open('data/' + str(i / records_per_file).zfill(6) + '.txt', 'w', 'utf-8')
+for record in warc_stream:
+    if file_name_counter % records_per_file == 0:
+        current_file = codecs.open('data/' + str(file_name_counter / records_per_file).zfill(6) + '.txt', 'w', 'utf-8')
     if record.type == "response":
         if record.content[1][0:50].find("HTTP/1.1 2") > -1: #if a succesful response
             startIndex = record.content[1].find("<html")
             if startIndex > -1: #if html found
 
-                fileObject.write(record.url + ' ')
+                current_file.write(record.url + ' ')
                 
                 
                 words = strip_html(record.content[1][startIndex:-1])
@@ -47,19 +47,19 @@ for record in warcStream:
                     # if word is utf-8, write it to file
                     try:
                         utf8_word = word.decode('utf-8')
-                        fileObject.write(utf8_word.lower() + ' ')
+                        current_file.write(utf8_word.lower() + ' ')
                     except UnicodeDecodeError:
                         pass
                 
-                fileObject.write('\n')
+                current_file.write('\n')
                 # logging
-                i = i + 1
-                if i % 100 == 0:
-                    print '%6s records processed' % (i)
-                if max_records > 0 and i == max_records:
+                file_name_counter = file_name_counter + 1
+                if file_name_counter % 100 == 0:
+                    print '%6s records processed' % (file_name_counter)
+                if max_records > 0 and file_name_counter == max_records:
                     break
     
-    if i % records_per_file == 0: 
-        fileObject.close()
+    if file_name_counter % records_per_file == 0: 
+        current_file.close()
 
 print 'Plain Text Files Have Been Extracted'
