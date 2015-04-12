9279c6c2b1f61cffd36c37a54c8f05cb90194975
Stefano Prezioso <stefprez@gmail.com>
2015-04-12 04:28:20 -0400

    warc_to_text conversion is now more thorough, faster, and output is more accurate.

--- src/warc_to_text.py
+++ src/warc_to_text.py
@@ -4,8 +4,11 @@ from hanzo import warctools
 import codecs
 import re
 import sys
-
+from spell_checker import SpellChecker
+from time import time
 from html_parser import strip_html
+
+start_time = time()
 
 if len(sys.argv) < 2:
     print "Please supply warc.gz file"
@@ -27,42 +30,50 @@ else:
 
 warc_stream = warctools.WarcRecord.open_archive(sys.argv[1])
 output_directory = sys.argv[2]
-regex = re.compile(r'\W+')
-
+spell_checker = SpellChecker()
 file_name_counter = 0
-
+current_file = codecs.open('data/' + str(file_name_counter / records_per_file)
+                           .zfill(6) + '.txt', 'w', 'utf-8')
 for record in warc_stream:
-    if file_name_counter % records_per_file == 0:
-        current_file = codecs.open(
-            'data/' + str(file_name_counter / records_per_file).zfill(6) +
-            '.txt', 'w', 'utf-8')
 
     if record.type == "response":
-        if record.content[1][0:50].find("HTTP/1.1 2") > -1:  # success
-            startIndex = record.content[1].find("<html")
 
-            if startIndex > -1:  # if html found
+        # if not a robots.txt page
+        if record.url.rfind('robots.txt') == -1:
+            # find start of <body>
+            startIndex = record.content[1][5:].find("<body")
 
+            # if body found
+            if startIndex > -1:
+                # add url to beginning of line
                 current_file.write(record.url + ' ')
 
+                # strip out html
                 words = strip_html(record.content[1][startIndex:-1])
+
                 for word in words:
-                    # if word is utf-8, write it to file
-                    try:
-                        utf8_word = word.decode('utf-8')
-                        current_file.write(utf8_word.lower() + ' ')
-                    except UnicodeDecodeError:
-                        pass
+                    if spell_checker.check(word):  # word is spelled correctly
+                        current_file.write(word + ' ')
 
                 current_file.write('\n')
-                # logging
-                file_name_counter = file_name_counter + 1
-                if file_name_counter % 500 == 0:
-                    print '%6s records processed' % (file_name_counter)
-                if max_records > 0 and file_name_counter == max_records:
-                    break
 
-    if file_name_counter % records_per_file == 0:
-        current_file.close()
+                # logging
+                file_name_counter += 1
+
+                if file_name_counter % 500 == 0:
+                    time_per_100_records = ((((time() - start_time) * 1000) /
+                                            file_name_counter) * 100)
+
+                    print '%6s records processed at %d ms per 100 records' % \
+                        (file_name_counter, time_per_100_records)
+
+                    if file_name_counter % records_per_file == 0:
+                        current_file.close()
+                        current_file = codecs.open(
+                            'data/' + str(file_name_counter / records_per_file)
+                            .zfill(6) + '.txt', 'w', 'utf-8')
 
 print 'Plain Text Files Have Been Extracted'
+
+minutes, seconds = divmod(time() - start_time, 60)
+print "Total Time: %d minutes %d seconds" % (minutes, seconds)
